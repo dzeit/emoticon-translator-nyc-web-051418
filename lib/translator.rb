2a921071ecd8f0b165836f40dbddb3e3b4903207
David Zeit <dzeit216@gmail.com>
2018-05-08 12:54:32 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,10 +6,10 @@ def load_library(emoticons)
   
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(emoticons)
   # code goes here
 end
 
-def get_english_meaning
+def get_english_meaning(emoticons)
   # code goes here
 end
