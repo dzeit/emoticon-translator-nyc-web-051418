3c3d73abaf70ad90909c013f0d41c3cd8aa047b1
David Zeit <dzeit216@gmail.com>
2018-05-08 15:45:42 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ require 'pry'
 
 def load_library(emoticons)
   emo = YAML.load_file(emoticons)
-  new_hash = {"get_meaning" => {}, "get_emoticon" => {"English" => , "Japanese"}}
+  new_hash = {"get_meaning" => {}, "get_emoticon" => {"English" => {},  "Japanese" => {}}}
   
   emo.each do |key, value|
     new_hash["get_emoticon"][key] = value 
