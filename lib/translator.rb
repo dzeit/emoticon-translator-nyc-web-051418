e6902d027972127ec90c02b26038b34f0a29d876
David Zeit <dzeit216@gmail.com>
2018-05-08 16:57:58 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,10 +8,9 @@ def load_library(emoticons)
   emo.each do |eng_translation, emoticons_array|
     eng_emoticon = emoticons_array[0]
     jap_emoticon = emoticons_array[1]
-    
     new_hash["get_emoticon"][eng_emoticon] = jap_emoticon
     new_hash["get_meaning"][jap_emoticon] = eng_translation
-     binding.pry  
+     #binding.pry  
   end 
   #binding.pry
   new_hash
