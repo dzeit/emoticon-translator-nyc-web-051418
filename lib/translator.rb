210bc79936e6841bcd39ceed6ddd6665a25f7933
David Zeit <dzeit216@gmail.com>
2018-05-08 15:27:07 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,7 @@ def load_library(emoticons)
   
   emo.each do |key, value|
     new_hash["get_emoticon"][key] = value 
-    binding.pry
+    #binding.pry
     value.each do |emoticon|
       new_hash["get_meaning"][emoticon] = key 
     end  
