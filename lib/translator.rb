414d8bf3a57bb80ebd58933bdddfed71f19f2336
David Zeit <dzeit216@gmail.com>
2018-05-08 18:07:08 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,8 +17,8 @@ end
 def get_japanese_emoticon(emo, emoticons)
   library = load_library(emo)
   library["get_emoticon"][emoticons]
-  sorry_message = 
-  return 
+  sorry_message = "Sorry, that emoticon was not found."
+  return sorry_message if != emoticons 
   #binding.pry 
 end
 
